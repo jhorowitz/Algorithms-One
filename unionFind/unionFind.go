b43cafd5c2a1d851aca317011ac92055a256eb5b
Josh Horowitz <joshua.m.horowitz@gmail.com>
2016-03-20 14:44:36 -0400

    init commit

---
+++ unionFind/unionFind.go
@@ -0,0 +1,31 @@
+package unionFind
+
+/*
+Given a set of N objects.
+ - Union command: connect two objects
+ - Find / connected query: Is there a path connecting the two objects
+
+
+Applications involve manipulating objects of all kinds
+ - Computers in a network
+ - Friends in a social network
+ - pixels in a photo
+ - transistors in a computer chip
+ - elements in a mathematical set
+ - variable names in a fortran program
+ - Metallic sites in a composite system
+
+
+We assume "is connected to" is an equivalence relation.
+ - Reflexive: p is connected to p
+ - Symmetric: If p is connected to q then q is connected to p
+ - Transitive: If p is connected to q and q is connected to r then p is connected to r
+
+Connected components: Maximal set of objects that are mutually connected
+
+ */
+
+func ReadFromStdIn() [][2]int {
+	return nil
+}
+
